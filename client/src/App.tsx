de221e568b93f8038d294cf119b0b2b4f7b4884c
IamAltay <etliekmek97@gmail.com>
2024-12-15 11:17:30 +0100

    package update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Header from './components/Header/Header.tsx'
 import Footer from './components/Footer/Footer.tsx'
 import CountButton from './components/Counter/CountButton.tsx'
+
 
 function App() {
 
